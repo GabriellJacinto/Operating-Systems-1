b7501d3d0a7a4ff33abdcf4ae195395cf73ba81c
GabrielLJacinto <gabriel.jacinto@pixforce.ai>
2023-03-15 16:47:25 -0300

    draft of the solution

--- cpu.cc
+++ cpu.cc
@@ -6,21 +6,27 @@ __BEGIN_API
 void CPU::Context::save()
 {
     //adicionar implementação
+    //set_context
 }
 
 void CPU::Context::load()
 {
     //adicionar implementação
+    //get_context
 }
 
 CPU::Context::~Context()
 {
-    //adicionar implementação
+    //cout << "Thread " << PID << "destruída"<<endl;
 }
 
 int CPU::switch_context(Context *from, Context *to)
 {
-     //implementação do método
+    //troca o contexto usando os dois
+    //contextos recebidos como parâmetro (de “from” para “to”) e retornar 0 caso seja bem sucedido
+    //ou negativo caso aconteça um erro.
+
+    
 }
 
 __END_API
