018ff9e70a09066f3369fed68e9b244797e74a26
carvalho <carvalho@lisha.ufsc.br>
2023-05-10 11:34:05 -0300

    Base code for T5

---
+++ T5/source/semaphore.cc
@@ -0,0 +1,41 @@
+#include "cpu.h"
+#include "debug.h"
+#include "traits.h"
+
+#include "semaphore.h"
+
+__BEGIN_API
+
+using namespace std;
+
+void Semaphore::p()
+{
+    // IMPLEMENTAR
+}
+
+void Semaphore::v()
+{
+    // IMPLEMENTAR
+}
+
+void Semaphore::sleep()
+{
+    // IMPLEMENTAR
+}
+
+void Semaphore::wakeup()
+{
+    // IMPLEMENTAR
+}
+
+void Semaphore::wakeup_all()
+{
+    // IMPLEMENTAR
+}
+
+Semaphore::~Semaphore()
+{
+    // IMPLEMENTAR
+}
+
+__END_API
