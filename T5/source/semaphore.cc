871496dbc2f8a6285c81d21b4723e47fdafc7ff9
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-05-25 18:56:55 -0300

    Writing TODO in methods

--- T5/source/semaphore.cc
+++ T5/source/semaphore.cc
@@ -10,27 +10,32 @@ using namespace std;
 
 void Semaphore::p()
 {
-    // IMPLEMENTAR
+    // Este método deve implementar a operacao p (ou sleep) de um semaforo. Deve-se decrementar o
+    // inteiro do semaforo de forma atomica (utilizando fdec descrita abaixo) e colocar a Thread para
+    // dormir caso a mesma nao conseguir acessar o semaforo (ja existir em uso por outra Thread).
 }
 
 void Semaphore::v()
 {
-    // IMPLEMENTAR
+    // Este metodo deve implementar a operacao v (ou wakeup) de um semaforo. Deve-se
+    // incrementar o inteiro do semaforo de forma atomica (utilizando finc descrita abaixo) e acordar
+    // uma Thread que estiver dormindo no semaforo.
 }
 
 void Semaphore::sleep()
 {
-    // IMPLEMENTAR
+    // O metodo sleep() deve colocar a Thread que nao conseguir acessar o semaforo para dormir e
+    // mudar seu estado para WAITING (note que WAITING eh diferente de SUSPENDED do trabalho anterior).
 }
 
 void Semaphore::wakeup()
 {
-    // IMPLEMENTAR
+    // O metodo wakeup() deve acordar uma Thread que estava dormindo no semaforo.
 }
 
 void Semaphore::wakeup_all()
 {
-    // IMPLEMENTAR
+    // O metodo wakeup_all() deve acordar todas as Thread que estavam dormindo no semaforo.
 }
 
 Semaphore::~Semaphore()
